2066beefc8d5f8b17ea06d755f53bc148c1f6b9b
Heriberto <106274607+HeryLB@users.noreply.github.com>
2024-12-10 07:44:40 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import server from "./server";
-import colors from "colors";
 
+const PORT = process.env.PORT || 4000;
 
-server.listen(4000, () => {
-    console.log(colors.cyan.bold("REST API en el puerto 4000"))
-})
+server.listen(PORT, () => {
+    console.log(colors.green(`Servidor corriendo en el puerto ${PORT}`));
+});
