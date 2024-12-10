364e4a391e1b76b1bfe79bef64491a440af06e98
Heriberto <106274607+HeryLB@users.noreply.github.com>
2024-12-10 07:52:44 -0600

    Update swagger.ts

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,25 +1,41 @@
-import swaggerJSDoc from "swagger-jsdoc";
-import { SwaggerOptions } from "swagger-ui-express";
+import swaggerJSDoc from 'swagger-jsdoc'
+import { SwaggerUiOptions } from 'swagger-ui-express'
 
 const options : swaggerJSDoc.Options = {
     swaggerDefinition: {
-        openapi: "3.0.0",
+        openapi: '3.0.2',
         tags: [
             {
-                name: "Products",
-                description: "API operations related to products"
+                name: 'Products',
+                description: 'API operations related to products'
             }
-            
-        ],
-        info:{
-            title: "REST API Node.js / Express / TypeScript",
+        ], 
+        info: {
+            title: 'REST API Node.js / Express / TypeScript',
             version: "1.0.0",
             description: "API Docs for Products"
         }
-    },
-    apis: ["./src/router.ts"]
+    }, 
+    apis: ['./src/router.ts']
 }
 
-const swaggerSpect = swaggerJSDoc(options)
+const swaggerSpec = swaggerJSDoc(options)
 
-export default swaggerSpect
+const swaggerUiOptions : SwaggerUiOptions = {
+    customCss : `
+        .topbar-wrapper .link {
+            content: url('https://codigoconjuan.com/wp-content/themes/cursosjuan/img/logo.svg');
+            height: 80px;
+            width: auto;
+        }
+        .swagger-ui .topbar {
+            background-color: #2b3b45;
+        }
+    `,
+    customSiteTitle: 'Documentación REST API Express / TypeScript'
+}
+
+export default swaggerSpec
+export {
+    swaggerUiOptions
+}
